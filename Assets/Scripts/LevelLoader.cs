8f7c90513f5699dcf8f763030d8639004ac53878
Konstantin Shevyakhov <Konstantin.Shevyakhov@kaspersky.com>
2019-03-13 20:21:47 +1000

    init

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private float timeToWait = 4f;
+    
+    private int _currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (_currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScreenWithDelay());
+        }
+    }
+
+    private IEnumerator LoadStartScreenWithDelay()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(_currentSceneIndex + 1);
+    }
+}
