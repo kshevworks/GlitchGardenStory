0349655770752128714219f64bc11077113bae31
Konstantin Shevyakhov <Konstantin.Shevyakhov@kaspersky.com>
2019-03-15 20:23:58 +1000

    implemented stars increasing

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -6,6 +6,17 @@ public class Defender : MonoBehaviour
 {
 
     [SerializeField] private int starCost = 100;
+
+    private StarDisplay _starDisplay;
     
+    private void Start()
+    {
+        _starDisplay = FindObjectOfType<StarDisplay>();
+    }
+
+    public void AddStars(int amount)
+    {
+        _starDisplay.AddStars(amount);
+    }
 
 }
